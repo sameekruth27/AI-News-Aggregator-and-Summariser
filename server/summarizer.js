4874c6af5f2e08811112927cdf480b35504d3c10
Sameekruth Talari <111893536+sameekruth27@users.noreply.github.com>
2023-12-05 14:47:56 +0530

    Add files via upload

---
+++ server/summarizer.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const { OpenAI } = require('openai');
+
+const apiKey = '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';
+const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
+
+const openai = new OpenAI(apiKey);
+
+
+const summarizeWithChatGPT = async (articleBody) => {
+  try {
+    const response = await openai.completions.create({
+      model: 'text-davinci-003', // Specify the GPT model here
+      prompt: `Summarize the following article:\n"${articleBody}"`,
+      max_tokens: 500,
+    });
+
+    const summary = response.choices[0].text.trim();
+    return summary;
+  } catch (error) {
+    console.error('Error summarizing with ChatGPT:', error);
+    return 'Failed to summarize';
+  }
+};
+
+
+module.exports = { summarizeWithChatGPT };
